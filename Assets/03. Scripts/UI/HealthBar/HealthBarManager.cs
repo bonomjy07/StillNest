3b52acff9be0c05cff25356d5175ad0223109387
meng <49272857+bonomjy07@users.noreply.github.com>
2025-07-10 20:41:49 +0900

    generic ui pool 대응

--- Assets/03. Scripts/UI/HealthBar/HealthBarManager.cs
+++ Assets/03. Scripts/UI/HealthBar/HealthBarManager.cs
@@ -1,53 +1,22 @@
+using System;
 using UnityEngine;
 using UnityEngine.Pool;
 
 public class HealthBarManager : Singleton<HealthBarManager>
 {
-    [SerializeField] private GameObject _healthBarPrefab;
-    [SerializeField] private Canvas _parentCanvas;
-        
-    private IObjectPool<HealthBar> _healthBarPool;
+    [SerializeField] private HealthBar _prefab;
+    [SerializeField] private Canvas _canvas;
+
+    private UIPool<HealthBar> _pool = new();
 
     protected override void Awake()
     {
         base.Awake();
-        _healthBarPool = new ObjectPool<HealthBar>(CreateHealthBar, OnGetHealthBar, OnReleaseHealthBar, OnDestroyHealthBar, false, 20, 100);
+
+        _pool.Initialize(_prefab, _canvas.transform);
     }
 
-    private HealthBar CreateHealthBar()
-    {
-        GameObject instance = Instantiate(_healthBarPrefab, _parentCanvas.transform);
-        if (instance && instance.TryGetComponent(out HealthBar healthBar))
-        {
-            healthBar.SetReleaseCallback(ReleaseHealthBar);
-            return healthBar;
-        }
-
-        return null;
-    }
-
-    private void OnGetHealthBar(HealthBar bar)
-    {
-        bar.gameObject.SetActive(true);
-    }
-
-    private void OnReleaseHealthBar(HealthBar bar)
-    {
-        bar.gameObject.SetActive(false);
-    }
-
-    private void OnDestroyHealthBar(HealthBar bar)
-    {
-        Destroy(bar.gameObject);
-    }
-
-    private void ReleaseHealthBar(HealthBar bar)
-    {
-        _healthBarPool.Release(bar);
-    }
-
-    public HealthBar GetHealthBar()
-    {
-        return _healthBarPool.Get();
-    }
+    public HealthBar Spawn() => _pool.Get();
+    
+    public void Despawn(HealthBar bar) => _pool.Release(bar);
 }
